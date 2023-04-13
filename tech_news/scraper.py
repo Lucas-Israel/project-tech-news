c2d53cd5a2fdc887763c24138dc82ec0bd836770
Lucas Israel <lisraelalves92@gmail.com>
2023-04-13 15:31:26 -0300

    feat: :sparkles: fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,19 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        header = {"user-agent": "Fake user-agent"}
+        result = requests.get(url, headers=header, timeout=3)
+    except requests.ReadTimeout:
+        return None
+    else:
+        if result.status_code == 200:
+            return result.text
+        return None
 
 
 # Requisito 2
@@ -21,3 +34,11 @@ def scrape_news(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+
+if __name__ == "__main__":
+    # URL = "https://app.betrybe.com/"
+    # URL = "https://blog.betrybe.com"
+    URL = "https://httpbin.org/delay/5"
+    a = fetch(URL)
+    print(a)
