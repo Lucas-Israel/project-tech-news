e550aa507b5d5fef401b98c70924e00af428f04b
Lucas Israel <lisraelalves92@gmail.com>
2023-04-14 17:04:57 -0300

    feat: :sparkles: added top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,22 @@
+from tech_news.database import get_collection
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    new_list = []
+    collection = get_collection()
+    pipeline = [
+        {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1, "_id": 1}},
+        {"$limit": 5},
+    ]
+    to_return = collection.aggregate(pipeline=pipeline)
+    for element in to_return:
+        new_list.append(element["_id"])
+    return new_list
+
+
+if __name__ == "__main__":
+    a = top_5_categories()
+    for b in a:
+        print(b)
